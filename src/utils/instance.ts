92089ee5bd12478870fd2b5a098ebb5559952ae7
John Livingston <git@john-livingston.fr>
2022-09-19 21:11:04 +0200

    Url validation.

--- src/utils/instance.ts
+++ src/utils/instance.ts
@@ -5,7 +5,11 @@ interface InstanceInfos {
 }
 
 async function getInstanceInfos (url: string): Promise<InstanceInfos | false> {
-  // TODO: validate that url is a valid url
+  // validate that url is a valid url:
+  const urlObject = new URL('', url)
+  if (!urlObject) {
+    throw new Error('Wrong url')
+  }
   // TODO: validate that url points to a Peertube server
   // TODO: validate that url points to a Peertube server allowing remote search
   // TODO: return correct infos
